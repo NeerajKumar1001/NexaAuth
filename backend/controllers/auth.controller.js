6bfb2400e5b3e320462428c9aa06e65e33237e45
Neeraj Kumar <144528779+NeerajKumar1001@users.noreply.github.com>
2025-07-24 08:02:30 +0530

    basic setup for backend with authentication# Initial commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signup = async(req, res) => {
+  res.send("signup Page");
+}
+
+export const login = async(req, res) => {
+  res.send("login Page");
+}
+
+export const logout = async(req, res) => {
+  res.send("logout Page");
+}
---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!'); 
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(3000, () => {
+    connectDB();
+    console.log('Connected to MongoDB');
+    console.log('Server is running on port 3000');
+});
+
+
+
+
